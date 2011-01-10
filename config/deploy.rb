589afcc270add63e7614216a76dbab69c4387e28
Shigeo Sakamot <shigeo.sakamoto@gmail.com>
2011-01-11 02:11:42 +0900

    add config/deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+set :application, "derrata"
+set :user, "derrata"
+set :repository,  "git://github.com/Shigeppon/derrata.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "183.181.175.0"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+set :deploy_to, "/home/#{application}"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+after "deploy:update_code", "deploy:link_images"
+namespace(:deploy) do
+  task :link_images do
+    run <<-CMD
+      cd #{release_path} &&
+      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
+      ln -nfs #{shared_path}/test/fixtures/admin_users.yml #{release_path}/test/fixtures/admin_users.yml
+    CMD
+  end
+end
+
+
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
